09329b7de10741018a2126e6b09049494f421c9d
GaEun132 <rkdms5813@gmail.com>
2025-08-12 14:06:43 +0900

    first springboot

---
+++ src/main/java/com/rookies4/myspringboot/MySpringBoot3ProjectApplication.java
@@ -0,0 +1,16 @@
+package com.rookies4.myspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//Application 클래스가 Configuration 클래스 역할을 한다. @SpringBootConfiguration
+//반드시 해당 클래스를 작성할 때 base-package 아래에 작성해야한다. @ComponentScan
+//외부 라이브러리에 대한 설정을 제공하는 AutoConfiguration을 활성화해주는 역할이다. @EnableAutoConfiguration
+@SpringBootApplication
+public class MySpringBoot3ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringBoot3ProjectApplication.class, args);
+	}
+
+}
